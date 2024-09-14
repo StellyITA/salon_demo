53527bb14a876c1d9f95b230add716af40dcef94
Stelly <stellyita@gmail.com>
2024-09-14 22:05:54 +0200

    Add "book appointment" functionality

--- src/App.java
+++ src/App.java
@@ -42,12 +42,14 @@ public class App {
                 System.out.println("\nWhat's your phone number?");
                 String phone = scannerObject.nextLine();
                 String customerName = "";
-                String selectCustomer = "SELECT name FROM customers WHERE phone = " + phone;
+                String customerID = "";
+                String selectCustomer = "SELECT name, customer_id FROM customers WHERE phone = " + phone;
 
                 try (ResultSet customerNameResultSet = statement.executeQuery(selectCustomer)) {
                     
                     customerNameResultSet.next();
                     customerName = customerNameResultSet.getString("name");
+                    customerID = customerNameResultSet.getString("customer_id");
                     System.out.println("Welcome back, " + customerName);
 
                 } catch (SQLException e) {
@@ -55,14 +57,29 @@ public class App {
                     customerName = scannerObject.nextLine();
                     String addCustomer = "INSERT INTO customers (phone,name) VALUES ('" + phone + "', '" + customerName + "')";
                     statement.executeUpdate(addCustomer);
+                    ResultSet customerNameResultSet = statement.executeQuery(selectCustomer);
+                    customerNameResultSet.next();
+                    customerID = customerNameResultSet.getString("customer_id");
                 }
 
-                break;
+                System.out.println("\nSelect a time for the service you would like to book (format hh:mm)");
+                String time = scannerObject.nextLine();
+                String addAppointment = "INSERT INTO appointments (customer_id, service_id, time) VALUES ('" + customerID + "', '" + chosenServiceID + "', '" + time + "')";
+                
+                try {
+                    statement.executeUpdate(addAppointment);
+                } catch (SQLException e) {
+                    System.out.println("Wrong time format.");
+                    continue;
+                }
+
+                System.out.println("\nI have put you down for a " + chosenService + " at " + time + ", " + customerName);
 
             } catch (SQLException e) {
                 e.printStackTrace();
-                break;
             }
+
+            break;
         }
 
     }
